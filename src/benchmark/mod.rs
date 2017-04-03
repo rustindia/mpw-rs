fac9b3ee28a9160bdfbe5740d043893912918871
Rahul De <rahul080327@gmail.com>
2017-04-03 12:47:41 +0530

    Gracefully fail for sysinfo

--- src/benchmark/mod.rs
+++ src/benchmark/mod.rs
@@ -34,6 +34,39 @@ fn calc_speed(elapsed: time::Duration, iterations: u32, operation: &str) -> f64 
     speed
 }
 
+fn show_sys_info() {
+    let os_type = match os_type() {
+        Ok(val) => val,
+        Err(_) => String::from("unknown")
+    };
+
+    let os_release = match os_release() {
+        Ok(val) => val,
+        Err(_) => String::from("unknown")
+    };
+
+    let cpu_num = match cpu_num() {
+        Ok(val) => val.to_string(),
+        Err(_) => String::from("unknown")
+    };
+
+    let cpu_speed = match cpu_speed() {
+        Ok(val) => val.to_string(),
+        Err(_) => String::from("unknown")
+    };
+
+    let mem_total = match mem_info() {
+        Ok(val) => (val.total / 1024).to_string(),
+        Err(_) => String::from("unknown")
+    };
+
+    println!("System Info:");
+    println!("OS: {} {}", os_type, os_release);
+    println!("CPU: {} cores at {} MHz/core", cpu_num, cpu_speed);
+    println!("Total Memory: {} MB", mem_total);
+    println!();
+}
+
 #[allow(unused_must_use)]
 pub fn mpw_bench() {
     let full_name = "Robert Lee Mitchel";
@@ -46,11 +79,7 @@ pub fn mpw_bench() {
     let algo = "3";
 
     println!("<<< Benchmarking Master Password >>>\n");
-    println!("System Info:");
-    println!("OS: {} {}", os_type().unwrap(), os_release().unwrap());
-    println!("CPU: {} cores at {} MHz/core", cpu_num().unwrap(), cpu_speed().unwrap());
-    println!("Total Memory: {} MB", mem_info().unwrap().total / 1024);
-    println!();
+    show_sys_info();
 
     let master_key = master_key_for_user(
         &full_name, &master_password, &algo, &site_variant).unwrap();
